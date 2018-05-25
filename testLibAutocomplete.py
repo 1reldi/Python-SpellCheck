63a3c5ef0519513700503550d194ca2ad5fb24f5
1reldi <reldi.cela@gmail.com>
2018-05-25 19:38:13 +0200

    predictive algorithm

--- testLibAutocomplete.py
+++
@@ -1,8 +0,0 @@
-import autocomplete
-
-# load pickled python Counter objects representing our predictive models
-# I use Peter Norvigs big.txt (http://norvig.com/big.txt) to create the predictive models
-autocomplete.load()
-
-# imagine writing "the b"
-autocomplete.predict('the','b')
